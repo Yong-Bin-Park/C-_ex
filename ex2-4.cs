e7f2e62101fbb991f77772ae2c456bb24f545b89
Yong-Bin-Park <90170614+Yong-Bin-Park@users.noreply.github.com>
2021-12-30 19:28:26 +0900

    Add files via upload

---
+++ ex2-4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+namespace ex2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("섭씨 온도값 입력: ");
+            int cel = int.Parse(Console.ReadLine());
+
+            double fah = (9 / 5.0) * cel + 32;
+
+            Console.WriteLine("화씨 온도값은: " + fah);
+
+        }
+}
+}
