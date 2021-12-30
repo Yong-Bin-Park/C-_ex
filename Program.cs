f5c6450f7f78ac6f83c6e23c1bf86cfab80a8318
Yong-Bin-Park <90170614+Yong-Bin-Park@users.noreply.github.com>
2021-12-30 19:26:42 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-
-
-namespace ex2_4
+namespace _2_5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("섭씨 온도값 입력: ");
-            int cel = int.Parse(Console.ReadLine());
+            int h, w;
 
-            double fah = (9 / 5.0) * cel + 32;
+            Console.WriteLine("키 입력:");
+            h = int.Parse(Console.ReadLine());
+            Console.WriteLine("몸무게 입력:");
+            w = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("화씨 온도값은: " + fah);
+            double sw = (h - 100) * 0.9;
+            double b = (w / sw) * 100;
+
+            Console.WriteLine("비만도는 " + b);
 
         }
 }
